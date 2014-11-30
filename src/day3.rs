87b6c832c1950ce095c1a2261f9ecd73d1f0bd3b
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-30 18:07:43 +0100

    Struct and Reader example.

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,14 @@
 extern crate csv;
+extern crate serialize;
 
-use csv::Writer;
+use csv::{Reader, Writer};
+
+#[deriving(Decodable, Encodable)]
+struct Movie {
+    title: String,
+    bad_guy: String,
+    pub_year: uint,
+}
 
 fn main() {
     println!("24 days of Rust - csv (day 3)");
@@ -13,5 +21,23 @@ fn main() {
     let mut writer = Writer::from_file(&path);
     for row in dollar_films.into_iter() {
         writer.encode(row).ok().expect("CSV writer error");
+    }
+    let movie = Movie {
+        title: "Hang 'Em High".to_string(),
+        bad_guy: "Wilson".to_string(),
+        pub_year: 1968u,
+    };
+    writer.encode(movie).ok().expect("CSV writer error");
+    writer.flush().ok().expect("Flush error");
+    let mut reader = Reader::from_file(&path).has_headers(false);
+    for row in reader.decode() {
+        let row: (String, String, uint) = row.unwrap();
+        println!("{}", row);
+    }
+    let mut reader = Reader::from_file(&path).has_headers(false);
+    for row in reader.decode() {
+        let movie: Movie = row.unwrap();
+        println!("{} was a bad guy in '{}' in {}",
+            movie.bad_guy, movie.title, movie.pub_year);
     }
 }
