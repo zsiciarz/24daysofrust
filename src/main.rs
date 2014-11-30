835f260ecfc08718560749165095ec320bb29d9a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-30 15:32:15 +0100

    Day 2 - slow_primes.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate slow_primes;
+
+use slow_primes::Primes;
+
+fn num_divisors(n: uint, primes: &Primes) -> Option<uint> {
+    use std::iter::MultiplicativeIterator;
+    match primes.factor(n) {
+        Ok(factors) => Some(factors.into_iter().map(|(_, x)| x + 1).product()),
+        Err(_) => None,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sieve = Primes::sieve(10000u);
+    let suspect = 5273u;
+    println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
+    let not_a_prime = 1024u;
+    println!("{} is prime: {}", not_a_prime, sieve.is_prime(not_a_prime));
+    let n = 1000u;
+    match sieve.primes().nth(n - 1) {
+        Some(number) => println!("{}th prime is {}", n, number),
+        None => println!("I don't know anything about {}th prime.", n),
+    }
+    println!("{}", sieve.factor(2610));
+    println!("{}", num_divisors(2610, &sieve));
 }
