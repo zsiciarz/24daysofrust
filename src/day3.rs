ddeb5ece1146580c1100024ac68d779a4ace2150
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-30 16:23:41 +0100

    First example of csv::Writer.

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,17 @@
+extern crate csv;
+
+use csv::Writer;
+
 fn main() {
     println!("24 days of Rust - csv (day 3)");
+    let dollar_films = vec![
+        ("A Fistful of Dollars", "Rojo", 1964u),
+        ("For a Few Dollars More", "El Indio", 1965u),
+        ("The Good, the Bad and the Ugly", "Tuco", 1966u),
+    ];
+    let path = Path::new("westerns.csv");
+    let mut writer = Writer::from_file(&path);
+    for row in dollar_films.into_iter() {
+        writer.encode(row).ok().expect("CSV writer error");
+    }
 }
