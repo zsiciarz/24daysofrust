1a2f9a22afd43b53e940a5fd53215ef7dfc339e4
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-02 20:50:42 +0100

    Switched to docopt! macro.

--- src/day4.rs
+++ src/day4.rs
@@ -1,9 +1,12 @@
+#![feature(phase)]
 extern crate serialize;
 extern crate docopt;
+#[phase(plugin)]
+extern crate docopt_macros;
 
 use docopt::Docopt;
 
-static USAGE: &'static str = "
+docopt!(Args, "
 Usage: wc [options] [<file>]
 
 Options:
@@ -14,25 +17,11 @@ Options:
     -L, --max-line-length  print the length of the longest line
     -h, --help  display this help and exit
     -v, --version  output version information and exit
-";
-
-#[deriving(Decodable)]
-struct Args {
-    arg_file: Option<String>,
-    flag_bytes: bool,
-    flag_chars: bool,
-    flag_lines: bool,
-    flag_words: bool,
-    flag_max_line_length: bool,
-}
+", arg_file: Option<String>)
 
 fn main() {
     println!("24 days of Rust - docopt (day 4)");
-    //let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
-    let docopt = match Docopt::new(USAGE) {
-        Ok(d) => d,
-        Err(e) => e.exit(),
-    };
+    let docopt = Args::docopt();
     println!("{}", docopt);
     let args: Args = match docopt.decode() {
         Ok(args) => args,
