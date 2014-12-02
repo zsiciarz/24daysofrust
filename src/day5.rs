f29a58aa3a8cc7384d881e24904b5c43e68c4007
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-03 00:09:55 +0100

    String-based post_query.

--- src/day5.rs
+++ src/day5.rs
@@ -2,6 +2,7 @@ extern crate hyper;
 
 use hyper::{HttpError, HttpResult, Url};
 use hyper::client::Request;
+use hyper::header::ContentLength;
 
 fn get_content(url: &str) -> HttpResult<String> {
     let url = match Url::parse(url) {
@@ -14,7 +15,21 @@ fn get_content(url: &str) -> HttpResult<String> {
     Ok(try!(response.read_to_string()))
 }
 
+fn post_query(url: &str, body: &str) -> HttpResult<String> {
+    let url = match Url::parse(url) {
+        Ok(url) => url,
+        Err(_) => return Err(HttpError::HttpUriError),
+    };
+    let mut fresh_request = try!(Request::post(url));
+    fresh_request.headers_mut().set(ContentLength(body.len()));
+    let mut streaming_request = try!(fresh_request.start());
+    try!(streaming_request.write_str(body));
+    let mut response = try!(streaming_request.send());
+    Ok(try!(response.read_to_string()))
+}
+
 fn main() {
     println!("24 days of Rust - hyper (day 5)");
     println!("{}", get_content("http://httpbin.org/status/200"));
+    println!("{}", post_query("http://httpbin.org/post", "key=value"));
 }
