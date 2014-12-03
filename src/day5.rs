d8ee0b55b9a38c072cabd6e6c5dba88ce7d5f4b9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-03 22:51:50 +0100

    Posting JSON with automatic serialization.

--- src/day5.rs
+++ src/day5.rs
@@ -1,9 +1,12 @@
 extern crate hyper;
+extern crate serialize;
 extern crate url;
 
 use hyper::{HttpError, HttpResult, Url};
 use hyper::client::Request;
 use hyper::header::ContentLength;
+use serialize::{Encodable, json};
+use std::io::IoError;
 use url::form_urlencoded;
 
 fn get_content(url: &str) -> HttpResult<String> {
@@ -33,9 +36,36 @@ fn post_query(url: &str, query: Query) -> HttpResult<String> {
     Ok(try!(response.read_to_string()))
 }
 
+fn post_json<'a, T: Encodable<json::Encoder<'a>, IoError>>(url: &str, payload: &T) -> HttpResult<String> {
+    let body = json::encode(payload);
+    let url = match Url::parse(url) {
+        Ok(url) => url,
+        Err(_) => return Err(HttpError::HttpUriError),
+    };
+    let mut fresh_request = try!(Request::post(url));
+    fresh_request.headers_mut().set(ContentLength(body.len()));
+    let mut streaming_request = try!(fresh_request.start());
+    try!(streaming_request.write_str(body[]));
+    let mut response = try!(streaming_request.send());
+    Ok(try!(response.read_to_string()))
+}
+
+#[deriving(Decodable, Encodable)]
+struct Movie {
+    title: String,
+    bad_guy: String,
+    pub_year: uint,
+}
+
 fn main() {
     println!("24 days of Rust - hyper (day 5)");
     println!("{}", get_content("http://httpbin.org/status/200"));
     let query = vec![("key", "value"), ("foo", "bar")];
     println!("{}", post_query("http://httpbin.org/post", query));
+    let movie = Movie {
+        title: "You Only Live Twice".to_string(),
+        bad_guy: "Blofeld".to_string(),
+        pub_year: 1967,
+    };
+    println!("{}", post_json("http://httpbin.org/post", &movie));
 }
