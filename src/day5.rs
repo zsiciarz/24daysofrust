97071b2ce734e270d2f70dbfe9af48f7eeccd812
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-03 22:10:39 +0100

    Fixed indentation.

--- src/day5.rs
+++ src/day5.rs
@@ -17,21 +17,21 @@ fn get_content(url: &str) -> HttpResult<String> {
     Ok(try!(response.read_to_string()))
 }
 
-    type Query<'a> = Vec<(&'a str, &'a str)>;
+type Query<'a> = Vec<(&'a str, &'a str)>;
 
-    fn post_query(url: &str, query: Query) -> HttpResult<String> {
-        let url = match Url::parse(url) {
-            Ok(url) => url,
-            Err(_) => return Err(HttpError::HttpUriError),
-        };
-        let body = form_urlencoded::serialize(query.into_iter());
-        let mut fresh_request = try!(Request::post(url));
-        fresh_request.headers_mut().set(ContentLength(body.len()));
-        let mut streaming_request = try!(fresh_request.start());
-        try!(streaming_request.write_str(body[]));
-        let mut response = try!(streaming_request.send());
-        Ok(try!(response.read_to_string()))
-    }
+fn post_query(url: &str, query: Query) -> HttpResult<String> {
+    let url = match Url::parse(url) {
+        Ok(url) => url,
+        Err(_) => return Err(HttpError::HttpUriError),
+    };
+    let body = form_urlencoded::serialize(query.into_iter());
+    let mut fresh_request = try!(Request::post(url));
+    fresh_request.headers_mut().set(ContentLength(body.len()));
+    let mut streaming_request = try!(fresh_request.start());
+    try!(streaming_request.write_str(body[]));
+    let mut response = try!(streaming_request.send());
+    Ok(try!(response.read_to_string()))
+}
 
 fn main() {
     println!("24 days of Rust - hyper (day 5)");
