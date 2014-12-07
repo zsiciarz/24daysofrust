edd6eb4b5682e49d7cabf3547c465a0d4ab08f00
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-07 19:14:59 +0100

    intersperse example

--- src/day7.rs
+++ src/day7.rs
@@ -9,4 +9,6 @@ fn main() {
     println!("24 days of Rust - itertools (day 7)");
     let mut words = "hello supercalifragilisticexpialidocious programmer".words();
     words.apply(|word| println!("{} is {} characters long", word, word.len()));
+    let it = range(1, 10u);
+    println!("{}", it.intersperse(15u).collect::<Vec<_>>());
 }
