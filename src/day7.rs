c750634d62a45abca98403112dd06a460b6162c1
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-07 19:14:46 +0100

    apply example

--- src/day7.rs
+++ src/day7.rs
@@ -3,6 +3,10 @@
 #[phase(plugin, link)]
 extern crate itertools;
 
+use itertools::Itertools;
+
 fn main() {
     println!("24 days of Rust - itertools (day 7)");
+    let mut words = "hello supercalifragilisticexpialidocious programmer".words();
+    words.apply(|word| println!("{} is {} characters long", word, word.len()));
 }
