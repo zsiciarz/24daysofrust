ef97733b4fa646c640d808fbfe4c98b66f89cd5a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-07 20:06:35 +0100

    Added interleave() example.

--- src/day7.rs
+++ src/day7.rs
@@ -9,6 +9,9 @@ fn main() {
     println!("24 days of Rust - itertools (day 7)");
     let mut words = "hello supercalifragilisticexpialidocious programmer".words();
     words.apply(|word| println!("{} is {} characters long", word, word.len()));
+    let even = range(1, 10u).map(|x| x * 2);
+    let odd = range(1, 5u).map(|x| x * 2 + 1);
+    println!("{}", even.interleave(odd).collect::<Vec<_>>());
     let it = range(1, 10u);
     println!("{}", it.intersperse(15u).collect::<Vec<_>>());
 }
