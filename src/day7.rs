c671c3c347b0c9b330e906e51675fc69161c89ff
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-07 20:44:14 +0100

    Macros example.

--- src/day7.rs
+++ src/day7.rs
@@ -3,6 +3,7 @@
 #[phase(plugin, link)]
 extern crate itertools;
 
+use std::iter::AdditiveIterator;
 use itertools::Itertools;
 
 fn main() {
@@ -14,4 +15,15 @@ fn main() {
     println!("{}", even.interleave(odd).collect::<Vec<_>>());
     let it = range(1, 10u);
     println!("{}", it.intersperse(15u).collect::<Vec<_>>());
+    let numbers = range(1, 4u);
+    let chars = vec!['a', 'b', 'c'];
+    for (i, c) in iproduct!(numbers, chars.iter()) {
+        println!("{}: {}", i, c);
+    }
+    let log = "GET / 4096\nGET /home/ 16301\nPOST /home/ 49\nGET / 4096\n";
+    let lines = log.lines();
+    let rows = icompr!(line.words().collect::<Vec<_>>() for line in lines);
+    let bytes = icompr!(row[2] for row in rows if row[0] != "POST");
+    let total = icompr!(from_str::<uint>(b).unwrap() for b in bytes).sum();
+    println!("Total GET throughput: {} bytes", total);
 }
