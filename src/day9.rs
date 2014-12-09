41eb3100baf89a9db594140b4925590ba2e714a9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-09 15:03:15 +0100

    Basic AnyMap example.

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,11 @@
+extern crate anymap;
+
+use anymap::AnyMap;
+
 fn main() {
     println!("24 days of Rust - anymap (day 9)");
+    let mut config = AnyMap::new();
+    config.insert("localhost");
+    println!("{}", config.get::<&str>());
+    println!("{}", config.get::<uint>());
 }
