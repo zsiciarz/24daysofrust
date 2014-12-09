461f170b2581035fc760db5bbd65b07003a103e3
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-09 23:57:08 +0100

    First attempt at getattr.

--- src/dayN_fuse.rs
+++ src/dayN_fuse.rs
@@ -1,11 +1,17 @@
 extern crate fuse;
+extern crate libc;
 
 use std::os;
-use fuse::Filesystem;
+use libc::{ENOENT, ENOSYS};
+use fuse::{FileAttr, Filesystem, Request, ReplyAttr, ReplyEntry, ReplyDirectory};
 
 struct JsonFilesystem;
 
 impl Filesystem for JsonFilesystem {
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        println!("getattr(): ino {}", ino);
+        reply.error(ENOSYS);
+    }
 }
 
 fn main() {
