d763b324c3cf7768f4616842be205f891d4edd5e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-10 17:09:20 +0100

    Math, math is good

--- src/day10.rs
+++ src/day10.rs
@@ -1,9 +1,16 @@
+extern crate num;
 extern crate tau;
 
 use std::num::FloatMath;
+use num::complex::{Complex, Complex64};
 
 fn main() {
     println!("24 days of Rust - tau (day 9)");
     println!("τ = {}", tau::TAU);
-    println!("cos(τ) = {}", tau::TAU.cos());
+    let radius: f64 = 15.0;
+    println!("circle circumference = τ * r = {}", tau::TAU * radius);
+    let c: Complex64 = Complex::from_polar(&1.0, &tau::TAU);
+    println!("Euler's identity: exp(i * τ) = {}", c);
+    println!("Trigonometry: sin(τ) = {}, cos(τ) = {}", tau::TAU.sin(), tau::TAU.cos());
+    println!("That other constant = {}", tau::TAU / 2.0);
 }
