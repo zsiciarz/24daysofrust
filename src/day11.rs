b995c2285326afc1861d24caeee7280934ccf6fe
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-11 15:55:43 +0100

    Initial PostgreSQL interaction.

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,19 @@
 extern crate postgres;
 
+use postgres::{Connection, SslMode};
+
 fn main() {
     println!("24 days of Rust - postgres (day 11)");
+    let dsn = "postgresql://rust:rust@localhost/rust";
+    let conn = match Connection::connect(dsn, &SslMode::None) {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("Connection error: {}", e);
+            return;
+        }
+    };
+    conn.execute("create table if not exists blog (
+        id serial primary key,
+        title varchar(255),
+        body text)", &[]).ok().expect("Create table failed");
 }
