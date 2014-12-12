0a02af76299dfa1ac750ef75eebc7ddfba118744
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-12 18:54:13 +0100

    Save as PNG.

--- src/day12.rs
+++ src/day12.rs
@@ -10,8 +10,8 @@ fn main() {
     println!("24 days of Rust - image (day 12)");
     let img = image::open(&Path::new("data/in.jpg")).ok().expect("Opening image failed");
     let filtered = img.fliph();
-    let out = File::create(&Path::new("out.jpg")).unwrap();
-    let _ = filtered.save(out, image::JPEG).ok().expect("Saving image failed");
+    let out = File::create(&Path::new("out.png")).unwrap();
+    let _ = filtered.save(out, image::PNG).ok().expect("Saving image failed");
 
     let kernel = [-1.0f32, -1.0, -1.0,
                   -1.0, 8.0, -1.0,
