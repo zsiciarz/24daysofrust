e954a9af50f92bc62919a52975f3b1bf5a34f01b
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-17 12:52:15 +0100

    fron_fn example.

--- src/day17.rs
+++ src/day17.rs
@@ -6,6 +6,10 @@ use std::io::File;
 
 fn main() {
     println!("24 days of Rust - patterns (day 17)");
+    let v = range(0, 10u).map(|x| x * 3).collect::<Vec<_>>();
+    println!("{}", v);
+    let v = Vec::from_fn(10, |x| x * 3);
+    println!("{}", v);
     let buffer = image::ImageBuffer::from_fn(512, 512, |x, y| {
         Pixel::from_channels((x * y % 256) as u8, (y % 256) as u8, (x % 256) as u8, 255)
     });
