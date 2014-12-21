a0cc7f9e249676af8483d91b8dda43ca5e17cd33
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-21 21:08:04 +0100

    Base64 encoding.

--- src/day21.rs
+++ src/day21.rs
@@ -1,7 +1,10 @@
 extern crate crypto;
+extern crate "rustc-serialize" as serialize;
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+
+use serialize::base64::{STANDARD, ToBase64};
 
 fn main() {
     println!("24 days of Rust - rust-crypto (day 21)");
@@ -9,4 +12,7 @@ fn main() {
     let mut sha = Sha256::new();
     sha.input_str(input);
     println!("{}", sha.result_str());
+    let mut bytes = Vec::from_elem(sha.output_bytes(), 0u8);
+    sha.result(bytes.as_mut_slice());
+    println!("{}", bytes.to_base64(STANDARD));
 }
