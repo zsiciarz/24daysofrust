e8a26aa2b42103d38dd6df669472b6009faf379f
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-21 21:55:07 +0100

    Almost complete AES-CTR example.

--- src/day21.rs
+++ src/day21.rs
@@ -1,10 +1,16 @@
+#![allow(unused_imports, unused_variables, unused_mut)]
+
 extern crate crypto;
 extern crate "rustc-serialize" as serialize;
 
+use crypto::aes::{mod, KeySize};
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use crypto::symmetriccipher::SynchronousStreamCipher;
 
 use serialize::base64::{STANDARD, ToBase64};
+
+use std::rand::{OsRng, Rng};
 
 fn main() {
     println!("24 days of Rust - rust-crypto (day 21)");
@@ -15,4 +21,17 @@ fn main() {
     let mut bytes = Vec::from_elem(sha.output_bytes(), 0u8);
     sha.result(bytes.as_mut_slice());
     println!("{}", bytes.to_base64(STANDARD));
+
+    let mut gen = OsRng::new().ok().expect("Failed to get OS random generator");
+    let mut key = Vec::from_elem(16, 0u8);
+    gen.fill_bytes(key.as_mut_slice());
+    let mut nonce = Vec::from_elem(16, 0u8);
+    gen.fill_bytes(nonce.as_mut_slice());
+    println!("Key: {}", key.to_base64(STANDARD));
+    println!("Nonce: {}", nonce.to_base64(STANDARD));
+    let mut cipher = aes::ctr(KeySize::KeySize128, key.as_slice(), nonce.as_slice());
+    let secret = "I like Nickelback";
+    let mut output = Vec::from_elem(secret.len(), 0u8);
+    //cipher.process(secret.as_bytes(), output.as_mut_slice());
+    //println!("{}", output.to_base64(STANDARD));
 }
