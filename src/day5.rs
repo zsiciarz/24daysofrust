cafbf67f56d3309983a650b8199a6496246ecdfe
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-12 17:06:55 +0100

    Bring back hyper example

--- src/day5.rs
+++ src/day5.rs
@@ -2,52 +2,30 @@ extern crate hyper;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate url;
 
-use hyper::{HttpError, HttpResult, Url};
-use hyper::client::Request;
-use hyper::header::ContentLength;
+use hyper::{Client, HttpResult};
 use rustc_serialize::{Encodable, json};
-use std::io::IoError;
 use url::form_urlencoded;
 
 fn get_content(url: &str) -> HttpResult<String> {
-    let url = match Url::parse(url) {
-        Ok(url) => url,
-        Err(err) => return Err(HttpError::HttpUriError(err)),
-    };
-    let fresh_request = try!(Request::get(url));
-    let streaming_request = try!(fresh_request.start());
-    let mut response = try!(streaming_request.send());
+    let mut client = Client::new();
+    let mut response = try!(client.get(url).send());
     Ok(try!(response.read_to_string()))
 }
 
 type Query<'a> = Vec<(&'a str, &'a str)>;
 
 fn post_query(url: &str, query: Query) -> HttpResult<String> {
-    let url = match Url::parse(url) {
-        Ok(url) => url,
-        Err(err) => return Err(HttpError::HttpUriError(err)),
-    };
+    let mut client = Client::new();
     let body = form_urlencoded::serialize(query.into_iter());
-    let mut fresh_request = try!(Request::post(url));
-    fresh_request.headers_mut().set(ContentLength(body.len()));
-    let mut streaming_request = try!(fresh_request.start());
-    try!(streaming_request.write_str(body[]));
-    let mut response = try!(streaming_request.send());
+    let mut response = try!(client.post(url).body(&body[]).send());
     Ok(try!(response.read_to_string()))
 }
 
 fn post_json<'a, T>(url: &str, payload: &T) -> HttpResult<String>
-    where T: Encodable<json::Encoder<'a>, IoError> {
+    where T: Encodable {
+    let mut client = Client::new();
     let body = json::encode(payload);
-    let url = match Url::parse(url) {
-        Ok(url) => url,
-        Err(err) => return Err(HttpError::HttpUriError(err)),
-    };
-    let mut fresh_request = try!(Request::post(url));
-    fresh_request.headers_mut().set(ContentLength(body.len()));
-    let mut streaming_request = try!(fresh_request.start());
-    try!(streaming_request.write_str(body[]));
-    let mut response = try!(streaming_request.send());
+    let mut response = try!(client.post(url).body(&body[]).send());
     Ok(try!(response.read_to_string()))
 }
 
@@ -55,18 +33,18 @@ fn post_json<'a, T>(url: &str, payload: &T) -> HttpResult<String>
 struct Movie {
     title: String,
     bad_guy: String,
-    pub_year: uint,
+    pub_year: usize,
 }
 
 fn main() {
     println!("24 days of Rust - hyper (day 5)");
-    println!("{}", get_content("http://httpbin.org/status/200"));
+    println!("{:?}", get_content("http://httpbin.org/status/200"));
     let query = vec![("key", "value"), ("foo", "bar")];
-    println!("{}", post_query("http://httpbin.org/post", query));
+    println!("{}", post_query("http://httpbin.org/post", query).unwrap());
     let movie = Movie {
         title: "You Only Live Twice".to_string(),
         bad_guy: "Blofeld".to_string(),
         pub_year: 1967,
     };
-    println!("{}", post_json("http://httpbin.org/post", &movie));
+    println!("{}", post_json("http://httpbin.org/post", &movie).unwrap());
 }
