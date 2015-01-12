5003ba703e93f687171eb0c109ff2117c1a7ad28
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-12 17:13:28 +0100

    (Partially) bring back Postgres example

--- src/day11.rs
+++ src/day11.rs
@@ -1,14 +1,14 @@
-#![feature(phase)]
+//#![feature(plugin)]
 
 extern crate "rustc-serialize" as serialize;
 extern crate postgres;
 
-#[phase(plugin)]
-extern crate postgres_macros;
+//#[plugin]
+//extern crate postgres_macros;
 
 use postgres::{Connection, Error, FromSql, SslMode};
 use postgres::Result as PgResult;
-use postgres::types::array::ArrayBase;
+//use postgres::types::array::ArrayBase;
 
 use serialize::json::Json;
 
@@ -27,7 +27,7 @@ fn main() {
     let conn = match Connection::connect(dsn, &SslMode::None) {
         Ok(conn) => conn,
         Err(e) => {
-            println!("Connection error: {}", e);
+            println!("Connection error: {:?}", e);
             return;
         }
     };
@@ -38,11 +38,11 @@ fn main() {
     let stmt = match conn.prepare("insert into blog (title, body) values ($1, $2)") {
         Ok(stmt) => stmt,
         Err(e) => {
-            println!("Preparing query failed: {}", e);
+            println!("Preparing query failed: {:?}", e);
             return;
         }
     };
-    for i in range(1, 5u) {
+    for i in 1..5 {
         let title = format!("Blogpost number {}", i);
         let text = format!("Content of the blogpost #{}", i);
         stmt.execute(&[&title, &text]).ok().expect("Inserting blogposts failed");
@@ -50,7 +50,7 @@ fn main() {
     let stmt = match conn.prepare("select id, title, body from blog where id < $1") {
         Ok(stmt) => stmt,
         Err(e) => {
-            println!("Preparing query failed: {}", e);
+            println!("Preparing query failed: {:?}", e);
             return;
         }
     };
@@ -61,16 +61,18 @@ fn main() {
         let title: String = row.get("title");
         println!("ID={}, title={}", id, title);
     }
-    println!("{}", get_single_value::<bool>(&conn, "select 1=1"));
-    println!("{}", get_single_value::<i32>(&conn, "select 1=1"));
+    println!("{:?}", get_single_value::<bool>(&conn, "select 1=1"));
+    println!("{:?}", get_single_value::<i32>(&conn, "select 1=1"));
+    /*
     type IntArray = ArrayBase<Option<i32>>;
     let arr = get_single_value::<IntArray>(&conn, "select '{4, 5, 6}'::int[]");
-    println!("{}", arr.map(|arr| arr.values()
+    println!("{:?}", arr.map(|arr| arr.values()
             .filter_map(|x| *x) // unwraps Some values and skips None
             .collect::<Vec<_>>()));
+    */
     let json = get_single_value::<Json>(&conn, "select '{\"foo\": \"bar\", \"answer\": 42}'::json");
-    println!("{}", json);
+    println!("{:?}", json);
 
-    let query = sql!("select '{4, 5, 6}'::int[]");
-    println!("{}", query);
+    //let query = sql!("select '{4, 5, 6}'::int[]");
+    //println!("{:?}", query);
 }
