9cc66e559b6651faaf4669580538f260f2442ebf
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-12 16:36:37 +0100

    Disabled icompr! macro example.

--- src/day7.rs
+++ src/day7.rs
@@ -10,18 +10,20 @@ fn main() {
     words.apply(|word| println!("{} is {} characters long", word, word.len()));
     let even = range(1, 10u).map(|x| x * 2);
     let odd = range(1, 5u).map(|x| x * 2 + 1);
-    println!("{}", even.interleave(odd).collect::<Vec<_>>());
+    println!("{:?}", even.interleave(odd).collect::<Vec<_>>());
     let it = range(1, 10u);
-    println!("{}", it.intersperse(15u).collect::<Vec<_>>());
+    println!("{:?}", it.intersperse(15u).collect::<Vec<_>>());
     let numbers = range(1, 4u);
     let chars = vec!['a', 'b', 'c'];
     for (i, c) in iproduct!(numbers, chars.iter()) {
         println!("{}: {}", i, c);
     }
+    /*
     let log = "GET / 4096\nGET /home/ 16301\nPOST /home/ 49\nGET / 4096\n";
     let lines = log.lines();
     let rows = icompr!(line.words().collect::<Vec<_>>() for line in lines);
     let bytes = icompr!(row[2] for row in rows if row[0] != "POST");
     let total = icompr!(b.parse::<uint>().unwrap() for b in bytes).sum();
     println!("Total GET throughput: {} bytes", total);
+    */
 }
