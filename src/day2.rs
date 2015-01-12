80d784e9f372edb83b296d1f21c048ced13af1ef
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-12 16:55:54 +0100

    Day 2 (slow_primes) is back

--- src/day2.rs
+++ src/day2.rs
@@ -2,7 +2,7 @@ extern crate slow_primes;
 
 use slow_primes::Primes;
 
-fn num_divisors(n: uint, primes: &Primes) -> Option<uint> {
+fn num_divisors(n: usize, primes: &Primes) -> Option<usize> {
     use std::iter::MultiplicativeIterator;
     match primes.factor(n) {
         Ok(factors) => Some(factors.into_iter().map(|(_, x)| x + 1).product()),
@@ -12,16 +12,16 @@ fn num_divisors(n: uint, primes: &Primes) -> Option<uint> {
 
 fn main() {
     println!("24 days of Rust - slow_primes (day 2)");
-    let sieve = Primes::sieve(10000u);
-    let suspect = 5273u;
+    let sieve = Primes::sieve(10000);
+    let suspect = 5273;
     println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
-    let not_a_prime = 1024u;
+    let not_a_prime = 1024;
     println!("{} is prime: {}", not_a_prime, sieve.is_prime(not_a_prime));
-    let n = 1000u;
+    let n = 1000;
     match sieve.primes().nth(n - 1) {
         Some(number) => println!("{}th prime is {}", n, number),
         None => println!("I don't know anything about {}th prime.", n),
     }
-    println!("{}", sieve.factor(2610));
-    println!("{}", num_divisors(2610, &sieve));
+    println!("{:?}", sieve.factor(2610));
+    println!("{:?}", num_divisors(2610, &sieve));
 }
