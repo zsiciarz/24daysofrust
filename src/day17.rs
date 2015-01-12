93ee6cd1dee3ca218a0efc03be08b725df9eb091
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-13 00:34:02 +0100

    Day 17 is back

--- src/day17.rs
+++ src/day17.rs
@@ -9,17 +9,15 @@ use std::rand;
 
 fn main() {
     println!("24 days of Rust - patterns (day 17)");
-    let v = range(0, 10u).map(|x| x * 3).collect::<Vec<_>>();
-    println!("{}", v);
-    let v = Vec::from_fn(10, |x| x * 3);
-    println!("{}", v);
-    let v = Vec::from_fn(10, |_| rand::random::<uint>());
-    println!("{}", v);
-    let mat: DMat<uint> = DMat::from_fn(7, 7, |i, j| if j <= i { 1 } else { 0 });
-    println!("{}", mat);
-    let buffer = image::ImageBuffer::from_fn(512, 512, |x, y| {
+    let v = (0..10).map(|x| x * 3).collect::<Vec<_>>();
+    println!("{:?}", v);
+    let v = (0..10).map(|_| rand::random::<u32>()).collect::<Vec<_>>();
+    println!("{:?}", v);
+    let mat: DMat<u32> = DMat::from_fn(7, 7, |i, j| if j <= i { 1 } else { 0 });
+    println!("{:?}", mat);
+    let buffer = image::ImageBuffer::from_fn(512u32, 512u32, Box::new(|&: x: u32, y: u32| {
         Pixel::from_channels((x * y % 256) as u8, (y % 256) as u8, (x % 256) as u8, 255)
-    });
+    }));
     let img = image::DynamicImage::ImageRgba8(buffer);
     let out = File::create(&Path::new("out_pattern.png")).unwrap();
     let _ = img.save(out, image::PNG);
