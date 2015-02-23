bed38ec2660e76cf1d83980cb7c900c696e80cd0
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-02-23 13:00:40 +0100

    Removed unnecessary Box from 17.

--- src/day17.rs
+++ src/day17.rs
@@ -17,9 +17,9 @@ fn main() {
     println!("{:?}", v);
     let mat: DMat<u32> = DMat::from_fn(7, 7, |i, j| if j <= i { 1 } else { 0 });
     println!("{:?}", mat);
-    let buffer = image::ImageBuffer::from_fn(512u32, 512u32, Box::new(|&: x: u32, y: u32| {
+    let buffer = image::ImageBuffer::from_fn(512u32, 512u32, |x: u32, y: u32| {
         Pixel::from_channels((x * y % 256) as u8, (y % 256) as u8, (x % 256) as u8, 255)
-    }));
+    });
     let img = image::DynamicImage::ImageRgba8(buffer);
     let mut out = File::create(&Path::new("out_pattern.png")).unwrap();
     let _ = img.save(&mut out, image::PNG);
