160f15d01aa4a10ba213e23b199fa05e1f8466a0
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-04-02 10:31:56 +0200

    Revived Redis example.

--- src/day18.rs
+++ src/day18.rs
@@ -1,16 +1,16 @@
+extern crate rand;
 extern crate redis;
 
 use redis::{Client, Commands, Connection, RedisResult};
 use std::collections::HashSet;
-use std::rand;
 
 type UserId = u64;
 
 fn add_friend(conn: &Connection, my_id: UserId, their_id: UserId) -> RedisResult<()> {
     let my_key = format!("friends:{}", my_id);
     let their_key = format!("friends:{}", their_id);
-    let _: () = try!(conn.sadd(my_key, their_id));
-    let _: () = try!(conn.sadd(their_key, my_id));
+    try!(conn.sadd(my_key, their_id));
+    try!(conn.sadd(their_key, my_id));
     Ok(())
 }
 
@@ -20,13 +20,13 @@ fn friends_in_common(conn: &Connection, my_id: UserId, their_id: UserId) -> Redi
     Ok(try!(conn.sinter((my_key, their_key))))
 }
 
-fn add_score(conn: &Connection, username: &str, score: uint) -> RedisResult<()> {
+fn add_score(conn: &Connection, username: &str, score: u32) -> RedisResult<()> {
     conn.zadd("leaderboard", username, score)
 }
 
-type Leaderboard = Vec<(String, uint)>;
+type Leaderboard = Vec<(String, u32)>;
 
-fn show_leaderboard(conn: &Connection, n: int) {
+fn show_leaderboard(conn: &Connection, n: isize) {
     let result: RedisResult<Leaderboard> = conn.zrevrange_withscores("leaderboard", 0, n - 1);
     match result {
         Ok(board) => {
@@ -43,11 +43,11 @@ fn main() {
     println!("24 days of Rust - redis (day 18)");
     let client = Client::open("redis://127.0.0.1/").unwrap();
     let conn = client.get_connection().unwrap();
-    let _: () = conn.set("answer", 42i).unwrap();
-    let answer: int = conn.get("answer").unwrap();
+    let _: () = conn.set("answer", 42).unwrap();
+    let answer: i32 = conn.get("answer").unwrap();
     println!("Answer: {}", answer);
 
-    for i in range(1, 10u64) {
+    for i in 1..10u64 {
         add_friend(&conn, i, i + 2).ok().expect("Friendship failed :(");
     }
     println!("You have {} friends in common.",
@@ -55,7 +55,7 @@ fn main() {
 
     let players = vec!["raynor", "kerrigan", "mengsk", "zasz", "tassadar"];
     for player in players.iter() {
-        let score = rand::random::<uint>() % 1000;
+        let score = rand::random::<u32>() % 1000;
         add_score(&conn, *player, score).ok().expect("Nuclear launch detected");
     }
     show_leaderboard(&conn, 3);
