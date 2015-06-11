2134b9cb77aa1838000838b3cb5784498bfd43d0
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-06-11 09:42:34 +0000

    update book at 9fbc593

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,8 @@
 extern crate primal;
 
-use primal::Primes;
+use primal::Sieve;
 
-fn num_divisors(n: usize, primes: &Primes) -> Option<usize> {
+fn num_divisors(n: usize, primes: &Sieve) -> Option<usize> {
     match primes.factor(n) {
         Ok(factors) => Some(factors.into_iter().fold(1, |acc, (_, x)| acc * (x + 1))),
         Err(_) => None,
@@ -11,13 +11,13 @@ fn num_divisors(n: usize, primes: &Primes) -> Option<usize> {
 
 fn main() {
     println!("24 days of Rust - primal (day 2)");
-    let sieve = Primes::sieve(10000);
+    let sieve = Sieve::new(10000);
     let suspect = 5273;
     println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
     let not_a_prime = 1024;
     println!("{} is prime: {}", not_a_prime, sieve.is_prime(not_a_prime));
     let n = 1000;
-    match sieve.primes().nth(n - 1) {
+    match sieve.primes_from(0).nth(n - 1) {
         Some(number) => println!("{}th prime is {}", n, number),
         None => println!("I don't know anything about {}th prime.", n),
     }
