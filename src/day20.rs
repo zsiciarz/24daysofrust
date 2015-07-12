f216bad09ed231f7c072fcbe8fa5f12109eebe8f
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-07-13 00:05:47 +0200

    First attempt at rewriting ZeroMQ example with rust-zmq.

--- src/day20.rs
+++ src/day20.rs
@@ -1,39 +1,36 @@
-extern crate zeromq;
+extern crate zmq;
 
-use zeromq::{Context, Msg, SocketType};
+use zmq::{Context, Message};
 
 fn main() {
-    println!("24 days of Rust - zeromq (day 20)");
-    let args = std::os::args();
+    println!("24 days of Rust - zmq (day 20)");
+    let args = std::env::args().collect::<Vec<_>>();
     if args.len() < 2 {
         println!("Usage: {} (client|server)", args[0]);
         return;
     }
-    let ctx = Context::new();
+    let mut ctx = Context::new();
     let addr = "tcp://127.0.0.1:25933";
     if args[1] == "client" {
         println!("ZeroMQ client connecting to {}", addr);
-        let mut sock = ctx.socket(SocketType::REQ);
+        let mut sock = ctx.socket(zmq::REQ).unwrap();
         let _ = sock.connect(addr);
-        let payload = "Hello world!".to_string();
-        println!("-> {}", payload);
-        let mut msg = box Msg::new(payload.len());
-        msg.data = payload.into_bytes();
-        let _ = sock.msg_send(msg);
-        if let Ok(msg) = sock.msg_recv() {
-            let contents = String::from_utf8(msg.data).ok().expect("Not a UTF-8 string");
-            println!("<- {}", contents);
-        }
+        let payload = b"Hello world!";
+        println!("-> {:?}", payload);
+        let mut msg = Message::new().unwrap();
+        sock.send(payload, 0).unwrap();
+        sock.recv(&mut msg, 0).unwrap();
+        let contents = msg.as_str();
+        println!("<- {:?}", contents);
     }
     else {
         println!("ZeroMQ server listening on {}", addr);
-        let mut sock = ctx.socket(SocketType::REP);
+        let mut sock = ctx.socket(zmq::REP).unwrap();
         let _ = sock.bind(addr);
+        let mut msg = Message::new().unwrap();
         loop {
-            if let Ok(msg) = sock.msg_recv() {
-                let mut reply = box Msg::new(msg.data.len());
-                reply.data = msg.data;
-                let _ = sock.msg_send(reply);
+            if let Ok(_) = sock.recv(&mut msg, 0) {
+                sock.send_str("hehehe", 0).unwrap();
             }
         }
     }
